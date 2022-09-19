94b18b3d52d6023ec747e6c01f78caf7ba024a26
Paulus <paulusmrvz@gmail.com>
2022-09-20 01:05:15 +0500

    modified premake file

--- premake5.lua
+++ premake5.lua
@@ -7,9 +7,8 @@ project "Volk"
     objdir    ("int/" .. outputdir .. "/%{prj.name}")
 
     files {
-        "include/glad/glad.h",
-        "include/KHR/khrplatform.h",
-        "src/glad.c"
+        "include/Volk/volk.h",
+        "src/volk.c"
     }
 
     includedirs {
